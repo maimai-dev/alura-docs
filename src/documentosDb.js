7bab4eb55db1aa939ce30aa68690c28b180ea608
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-10-25 14:49:52 -0300

    video-5.1

--- src/documentosDb.js
+++ src/documentosDb.js
@@ -1,5 +1,10 @@
 import { documentosColecao } from "./dbConnect.js";
 
+function obterDocumentos() {
+  const documentos = documentosColecao.find().toArray();
+  return documentos;
+}
+
 function encontrarDocumento(nome) {
   const documento = documentosColecao.findOne({
     nome,
@@ -23,4 +28,4 @@ function atualizaDocumento(nome, texto) {
   return atualizacao;
 }
 
-export { encontrarDocumento, atualizaDocumento };
+export { encontrarDocumento, atualizaDocumento, obterDocumentos };
