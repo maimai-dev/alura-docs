90edf0270ef57c492f627168ce3bcc0a51c89000
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-10-25 16:20:35 -0300

    video-5.5

--- public/documento.js
+++ public/documento.js
@@ -30,4 +30,11 @@ botaoExcluir.addEventListener("click", () => {
   emitirExcluirDocumento(nomeDocumento);
 });
 
-export { atualizaTextoEditor };
+function alertarERedirecionar(nome) {
+  if (nome === nomeDocumento) {
+    alert(`Documento ${nome} excluído!`);
+    window.location.href = "/";
+  }
+}
+
+export { atualizaTextoEditor, alertarERedirecionar };
