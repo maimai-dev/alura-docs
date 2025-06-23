526cade3f154dc6a4d518e4dfa104f02762ecadf
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-06-23 17:12:28 -0300

    feat:adicionado armazenamento local

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,5 +1,20 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de node..."
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io..."
+    }
+]
+
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou! ID:', socket.id);
 
@@ -7,13 +22,32 @@ io.on('connection', (socket) => {
         console.log(`Cliente ${socket.id} desconectado! Motivo: ${motivo}`)
     });
 
-        socket.on('selecionar_documento', (nome_documento) => {
-        socket.join(nome_documento)
+        socket.on('selecionar_documento', (nome_documento, devolverTexto) => {
+            socket.join(nome_documento)
+            const documento = encontrarDocumento(nome_documento);
+            
+            if(documento){
+                devolverTexto(documento.texto);
+            }
+
     });
 
     socket.on('texto_editor', ({texto, nomeDocumento}) => {
-        socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if(documento){
+            documento.texto = texto;
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+        }
     });
 
-})
+});
+
+function encontrarDocumento (nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome;
+    });
+
+    return documento;
+}
 
