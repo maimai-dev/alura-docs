396b0f274aed7f64df8b0773c67455061a11ca06
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-06-24 13:41:52 -0300

    minor changes

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const cliente = new MongoClient("mongodb+srv://admin:admin123@aluracluster.4qemetw.mongodb.net/?retryWrites=true&w=majority&appName=AluraCluster");
+const cliente = new MongoClient("");
 
 try {
     await cliente.connect();
