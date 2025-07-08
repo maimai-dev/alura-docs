92d0daf720e9ddc5c5db8dfd2f5fc109373040ae
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-07-07 23:42:25 -0300

    feat: adicionado conexão com mongo db

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,14 +1,19 @@
 import { MongoClient } from "mongodb";
+import 'dotenv/config';
 
-const cliente = new MongoClient("");
+const cliente = new MongoClient(process.env.MONGO_DB_STRING);
+
+let documentosColecao;
 
 try {
     await cliente.connect();
 
     const db = cliente.db("alura-websockets");
-    const documentos = db.collection("documentos");
+    documentosColecao = db.collection("documentos");
 
     console.log("Conectado ao banco de dados com sucesso!");
 } catch (erro) {
     console.log(erro);
 }
+
+export {documentosColecao};
